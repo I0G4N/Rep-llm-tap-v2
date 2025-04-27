174b968721f12560e3b66f024a13291683e072c4
I0G4N <logan.zh.cai@gmail.com>
2025-04-27 22:39:54 +0800

    添加 FeedForward 类以实现前馈神经网络功能

--- transformer.py
+++ transformer.py
@@ -108,3 +108,19 @@ class MultiHeadAttention(nn.Module):
         output = self.out(concat) # (batch_size, seq_len, d_model)
 
         return output
+
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super().__init__()
+
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(d_ff, d_model)
+    
+    def forward(self, x):
+        x = self.dropout(F.relu(self.linear1(x)))
+        x = self.linear2(x)
+
+        return x
+
